9ec5e07e5e4b70534d1b4b4157900d7d27b9226b
fz1998 <sunday123>
2015-08-18 07:00:06 +0800

    commit everything under HelloAndroid2

---
+++ mylibrary/src/main/java/study/zou/frank/mylibrary/Hello.java
@@ -0,0 +1,12 @@
+package study.zou.frank.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by htyu on 2015/8/18.
+ */
+public class Hello {
+    public static void hi() {
+        Log.d("HelloAndroid", "Hi there.");
+    }
+}
---
+++ mylibrary/src/main/res/Util.java
@@ -0,0 +1,8 @@
+/**
+ * Created by htyu on 2015/8/18.
+ */
+public class Util {
+    public static void Hi() {
+        
+    }
+}
